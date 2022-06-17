7ea80bd4d1bcc1cabf57ab13d7a09f1aade3eeea
sangarian <107142232+sangarian@users.noreply.github.com>
2022-06-17 09:29:38 +0100

    Create dashboard.R

---
+++ dashboard.R
@@ -0,0 +1,22 @@
+"    .............................  NOTE......................................
+
+DOWNLOAD THE SAMPLE DATA FROM THE FOLDER (Sample Data) AND SAVE IT IN YOUR WORKING DIRECTORY"
+
+
+
+# load the required packages
+
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library (tidyr)
+library(shinythemes) 
+library("stringr")
+library (reshape2)
+library(kableExtra)
+library(knitr)
+library(tidyverse)
+library(lubridate)
+library(readr)
+
